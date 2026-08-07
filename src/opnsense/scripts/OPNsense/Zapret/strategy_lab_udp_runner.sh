f14730540df94e1b9459dccda0c911987c343ffc
Tolian82 <309637356+Tolian82@users.noreply.github.com>
2026-08-08 00:52:11 +0300

    v0.3.3_20: Restore job-local UDP input at stage runner

--- src/opnsense/scripts/OPNsense/Zapret/strategy_lab_udp_runner.sh
+++ src/opnsense/scripts/OPNsense/Zapret/strategy_lab_udp_runner.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 SCRIPT_DIR="${SCRIPT_DIR:-/usr/local/opnsense/scripts/OPNsense/Zapret}"; MODULE_DIR="${MODULE_DIR:-${SCRIPT_DIR}/strategy_lab}"
 set -eu
-for module in common udp; do path="${MODULE_DIR}/${module}.sh"; [ -r "${path}" ] || exit 1; . "${path}"; done
+for module in common state udp udp_input; do path="${MODULE_DIR}/${module}.sh"; [ -r "${path}" ] || exit 1; . "${path}"; done
 strategy_lab_require_jq
 JOB_ID="$1"; ENDPOINTS_FILE="$2"; RESULT_FILE="$3"
 strategy_lab_job_id_valid "${JOB_ID}" || exit 64
 [ -r "${ENDPOINTS_FILE}" ] || exit 64
+strategy_lab_udp_input_export "${JOB_ID}" || exit 1
 strategy_lab_udp_run "${JOB_ID}" "${ENDPOINTS_FILE}" "${RESULT_FILE}"
