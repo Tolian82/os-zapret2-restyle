0c5fda370efdfd4f0c25026d3b84ee5cda5efbcd
Tolian82 <309637356+Tolian82@users.noreply.github.com>
2026-08-08 08:18:33 +0300

    v0.3.3_23: route parameter expansion to Python

--- src/opnsense/scripts/OPNsense/Zapret/strategy_lab_expansion_runner.sh
+++ src/opnsense/scripts/OPNsense/Zapret/strategy_lab_expansion_runner.sh
@@ -1,29 +1,7 @@
 #!/bin/sh
-
 SCRIPT_DIR="${SCRIPT_DIR:-/usr/local/opnsense/scripts/OPNsense/Zapret}"
-MODULE_DIR="${MODULE_DIR:-${SCRIPT_DIR}/strategy_lab}"
-
+PYTHON_LAUNCHER="${STRATEGY_LAB_PYTHON_LAUNCHER:-${SCRIPT_DIR}/strategy_lab_python_launcher.sh}"
 set -eu
-umask 022
-
-for module in common expansion
-do
-    module_path="${MODULE_DIR}/${module}.sh"
-    [ -r "${module_path}" ] || {
-        echo "ERROR: required Strategy Lab expansion module is missing: ${module_path}" >&2
-        exit 1
-    }
-    . "${module_path}"
-done
-
-strategy_lab_require_jq
-JOB_ID="${1:-}"
-ENDPOINTS_FILE="${2:-}"
-FAMILY_RESULT_FILE="${3:-}"
-RESULT_FILE="${4:-}"
-strategy_lab_job_id_valid "${JOB_ID}" || exit 64
-[ -r "${ENDPOINTS_FILE}" ] || exit 64
-[ -r "${FAMILY_RESULT_FILE}" ] || exit 64
-[ -n "${RESULT_FILE}" ] || exit 64
-
-strategy_lab_expansion_run "${JOB_ID}" "${ENDPOINTS_FILE}" "${FAMILY_RESULT_FILE}" "${RESULT_FILE}"
+[ "$#" -eq 4 ] || exit 64
+[ -x "${PYTHON_LAUNCHER}" ] || exit 69
+exec "${PYTHON_LAUNCHER}" search expand "$1" "$2" "$3" "$4"
