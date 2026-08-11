3d6a9213a9eb59162663a316779ebe6e8a0dc091
Tolian82 <309637356+Tolian82@users.noreply.github.com>
2026-08-11 16:26:47 +0300

    v0.4.0_22: route production Stage 60 through parallel owner

--- src/opnsense/scripts/OPNsense/Zapret/strategy_lab_expansion_runner.sh
+++ src/opnsense/scripts/OPNsense/Zapret/strategy_lab_expansion_runner.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 SCRIPT_DIR="${SCRIPT_DIR:-/usr/local/opnsense/scripts/OPNsense/Zapret}"
-PYTHON_LAUNCHER="${STRATEGY_LAB_PYTHON_LAUNCHER:-${SCRIPT_DIR}/strategy_lab_python_launcher.sh}"
+STAGE60_RUNNER="${STRATEGY_LAB_STAGE60_RUNNER:-${SCRIPT_DIR}/strategy_lab_stage60_parallel_runner.sh}"
 set -eu
 [ "$#" -eq 4 ] || exit 64
-[ -x "${PYTHON_LAUNCHER}" ] || exit 69
-exec "${PYTHON_LAUNCHER}" search expand "$1" "$2" "$3" "$4"
+[ -x "${STAGE60_RUNNER}" ] || exit 69
+exec "${STAGE60_RUNNER}" "$@"
